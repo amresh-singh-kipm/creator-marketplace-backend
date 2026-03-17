7b75c0fa39719e89a4284e1a4c772d888c6dae83
Amresh Singh <amresh.singh@qbslearning.com>
2026-03-17 18:08:19 +0530

    urls added in cors

--- index.js
+++ index.js
@@ -9,7 +9,11 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://mydigitalassets.store",
+      "https://api.mydigitalassets.store",
+    ],
     credentials: true,
   }),
 );
