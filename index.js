e9e3f3e4a9e7820ea8964596cd28fb436d3ea9f1
Amresh Singh <amresh.singh@qbslearning.com>
2026-03-17 18:30:29 +0530

    removed *

--- index.js
+++ index.js
@@ -7,17 +7,18 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(
-  cors({
-    origin: [
-      // "http://localhost:5173",
-      // "https://mydigitalassets.store",
-      // "https://api.mydigitalassets.store",
-      "*",
-    ],
-    credentials: true,
-  }),
-);
+// app.use(
+//   cors({
+//     origin: [
+//       // "http://localhost:5173",
+//       // "https://mydigitalassets.store",
+//       // "https://api.mydigitalassets.store",
+//       "*",
+//     ],
+//     credentials: true,
+//   }),
+// );
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
